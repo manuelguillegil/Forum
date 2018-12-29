f55bd90cae3e5a1074cf41502f19f5fa35aeeac2
manuelguillermogil <manuelguillermogil@gmail.com>
2018-12-29 03:07:28 +0100

    Estoy iniciando un nuevo repositorio al proyecto de Django

---
+++ myproject/boards/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from .models import Board
+
+def home(request):
+	boards = Board.objects.all()
+	return render(request,'home.html',{'boards':boards})
